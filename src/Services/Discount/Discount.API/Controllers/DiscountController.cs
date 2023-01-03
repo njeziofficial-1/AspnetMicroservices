8ea98f80b1190a38d9074f8ec0286442567554cd
John Njezi <john.njezi@toyingroupsystems.com>
2023-01-03 02:35:45 +0100

    Implemented database resillience by recursion and completed application, waiting to test on Docker

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,44 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _repository;
+
+        public DiscountController(IDiscountRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("{productName}", Name = "GetDiscount")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetDiscount(string productName)
+        => Ok(await _repository.GetDiscount(productName));
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await _repository.CreateDiscount(coupon);
+            return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateDiscount([FromBody]Coupon coupon)
+        => Ok(await _repository.UpdateDiscount(coupon));
+
+        [HttpDelete("{productName}", Name = "DeleteDiscount")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteDiscount(string productName)
+        => Ok(await _repository.DeleteDiscount(productName));
+    }
+}
---
+++ src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,61 @@
+﻿using Dapper;
+using Discount.API.Entities;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public class DiscountRepository : IDiscountRepository, IDisposable
+    {
+        private readonly IConfiguration _configuration;
+
+        NpgsqlConnection _connection { get; }
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        }
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            var affected = await _connection.ExecuteAsync
+                ("INSERT INTO Coupon(ProductName, Description, Amount) VALUES(@ProductName, @Description, @Amount)",
+                new { coupon.ProductName, coupon.Description, coupon.Amount });
+            return affected != 0;
+        }
+
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            var affected = await _connection.ExecuteAsync
+                ("DELETE FROM Coupon WHERE ProductName=@ProductName",
+                new { ProductName = productName });
+            return affected != 0;
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+           var coupon = await _connection.QueryFirstOrDefaultAsync<Coupon>
+                ("SELECT * FROM Coupon WHERE ProductName = @ProductName", new { ProductName = productName});
+            if (coupon is null)
+                return new Coupon
+                {
+                    ProductName = "No Discount",
+                    Amount = 0,
+                    Description = "No Discount Desc."
+                };
+
+            return coupon;
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            var affected = await _connection.ExecuteAsync
+                ("UPDATE Coupon SET ProductName=@ProductName, Description=@Description, Amount=@Amount WHERE Id=@Id",
+                new { coupon.ProductName, coupon.Description, coupon.Amount, coupon.Id });
+            return affected != 0;
+        }
+
+        void IDisposable.Dispose() => _connection.Dispose();
+    }
+}
