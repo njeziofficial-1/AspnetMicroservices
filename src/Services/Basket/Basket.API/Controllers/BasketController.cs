64c32867a258e0c9d6e36dc742c6354b4f808a9a
John Njezi <john.njezi@toyingroupsystems.com>
2023-01-08 02:25:08 +0100

    Implemented CQRS for Order

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -13,10 +14,12 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
+        private readonly IDiscountGrpcService _discountGrpcService;
 
-        public BasketController(IBasketRepository repository)
+        public BasketController(IBasketRepository repository, IDiscountGrpcService discountGrpcService)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
         }
 
         [HttpGet("{userName}", Name = "GetBasket")]
@@ -30,7 +33,16 @@ namespace Basket.API.Controllers
         [HttpPost]
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> UpdateBasket([FromBody] ShoppingCart basket)
-        => Ok(await _repository.UpdateBasket(basket));
+        {
+            //TODO: Communicate with  Discount.Grpc
+            //Calculate latest prices of product into shopping cart.
+            foreach (var item in basket.Items)
+            {
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount;
+            }
+            return Ok(await _repository.UpdateBasket(basket));
+        }
 
         [HttpDelete("{userName}", Name = "DeleteBasket")]
         [ProducesResponseType((int)HttpStatusCode.OK)]
--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -1,6 +1,3 @@
-using Basket.API.GrpcServices;
-using Basket.API.Repositories;
-using Discount.Grpc.Protos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -14,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Basket.API
+namespace Ordering.API
 {
     public class Startup
     {
@@ -32,21 +29,8 @@ namespace Basket.API
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ordering.API", Version = "v1" });
             });
-
-            //===> Configure Redis
-            services.AddStackExchangeRedisCache(option =>
-            {
-                option.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
-            });
-
-            //===> Register Dependencies
-            services.AddScoped<IBasketRepository, BasketRepository>();
-            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
-                (o => o.Address = new Uri(Configuration.GetValue<string>("GrpcSettings:DiscountUri")));
-            services.AddScoped<IDiscountGrpcService, DiscountGrpcService>();
-            //
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,7 +40,7 @@ namespace Basket.API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ordering.API v1"));
             }
 
             app.UseRouting();
