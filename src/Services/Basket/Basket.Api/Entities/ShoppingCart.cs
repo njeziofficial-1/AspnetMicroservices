6567332dfec262bb329345e425145248b2e17305
John Njezi <john.njezi@toyingroupsystems.com>
2024-07-25 12:35:00 +0100

    Completed Catalog

---
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,34 @@
+﻿namespace Basket.Api.Entities;
+
+public class ShoppingCart
+{
+    public ShoppingCart()
+    {
+
+    }
+    public ShoppingCart(string? userName)
+    {
+        UserName = userName;
+    }
+    public string? UserName { get; set; }
+    public List<ShoppingCartItem> Items { get; set; } = [];
+    public double TotalPrice
+    {
+        get
+        {
+            double totalPrice = 0;
+            foreach (var item in Items)
+                totalPrice += item.Price * item.Quantity;
+            return totalPrice;
+        }
+    }
+
+    public class ShoppingCartItem
+    {
+        public int Quantity { get; set; }
+        public string? Color { get; set; }
+        public double Price { get; set; }
+        public string? ProductId { get; set; }
+        public string? ProductName { get; set; }
+    }
+}
---
+++ src/Services/Basket/Basket.Api/Repositories/BasketRepository.cs
@@ -0,0 +1,23 @@
+﻿using Basket.Api.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Basket.Api.Repositories;
+
+public class BasketRepository : IBasketRepository
+{
+    readonly IDistributedCache _redisCache;
+    public Task DeleteBasket(string userName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ShoppingCart> GetBasket(string userName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+    {
+        throw new NotImplementedException();
+    }
+}
