4d312a552682bc5249f773efd77c0e41fce6ee52
njeziofficial-1 <john.njezi@toyingroupsystems.com>
2024-10-17 06:35:58 +0100

    Created Persistence in Infrastucture

---
+++ src/Services/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net;
+
+namespace Ordering.Infrastructure.Mail;
+
+public class EmailService : IEmailService
+{
+    public EmailSettings _emailSettings { get; }
+    public ILogger<EmailService> _logger { get; }
+
+    public EmailService(IOptions<EmailSettings> emailSettings, ILogger<EmailService> logger)
+    {
+        _emailSettings = emailSettings.Value;
+        _logger = logger;
+    }
+
+    public async Task<bool> SendEmailAsync(Email email)
+    {
+        var client = new SendGridClient(_emailSettings.ApiKey);
+        var subject = email.Subject;
+        var emailBody = email.Body;
+        EmailAddress to = new(email.To);
+        var from = new EmailAddress
+        {
+            Email = _emailSettings.FromAddress,
+            Name = _emailSettings.FromName
+        };
+
+        var sendMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+        var response = await client.SendEmailAsync(sendMessage);
+        _logger.LogInformation("Email sent");
+
+        if(response.StatusCode == HttpStatusCode.Accepted || response.StatusCode == HttpStatusCode.OK)
+            return true;
+        _logger.LogError("Email sending fail");
+        return false;
+    }
+}
